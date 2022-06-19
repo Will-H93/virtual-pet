e85544e56fcdd80fef9b62ad80a31d68423a30ae
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-06-19 21:06:58 +0100

    Added walk function and test passes, chose not to refactor further as unnecessary.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,16 +12,28 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
   
       expect(pet.age).toEqual(0);
-    });
-    describe('growUp', () => {
-      it('increments the age by 1, hunger by 5 and decreases fitness by 3', () => {
-        const pet = new Pet('Fido');
+    })
+});
+
+describe('growUp', () => {
+  it('increments the age by 1, hunger by 5 and decreases fitness by 3', () => {
+    const pet = new Pet('Fido');
     
-        pet.growUp();
+    pet.growUp();
     
-        expect(pet.age).toEqual(1);
-        expect(pet.hunger).toEqual(5);
-        expect(pet.fitness).toEqual(7);
-      });
+    expect(pet.age).toEqual(1);
+    expect(pet.hunger).toEqual(5);
+    expect(pet.fitness).toEqual(7);
   });
-})
+});
+
+describe('walk', () => {
+  it('increments fitness by 4 or equals maximun fitness if fitness will be greater than 10', () => {
+    const pet = new Pet('fido');
+
+    pet.fitness = 8;
+    pet.walk();
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
